749d5c8d01144daffb4956ec70c3e8ce5065261c
Chelsea Voss <csvoss@mit.edu>
2014-09-29 21:06:04 -0400

    Aesthetic improvements
    Cursors. Paragraph of description text. Species names. Expand and collapse of sidebar. Smaller thumbnails. Better font.

--- indexgen.py
+++ indexgen.py
@@ -14,15 +14,23 @@ node_urls = {}
 link_urls = {}
 linkcounter = {}
 
+node_species={}
+link_species={}
+
 ##Read the input files
 for line in nodefile:
     stuff = line.split(' ')
+    if len(stuff) < 2:
+        continue
     node_urls[stuff[0]] = stuff[1].strip()
+    node_species[stuff[0]] = ' '.join(stuff[2:]).strip()
 
 nodefile.close()
 
 for line in linkfile:
     stuff = line.split(' ')
+    if len(stuff) < 3:
+        continue
     num = 0
     tup = (stuff[0], stuff[1])
     if tup in linkcounter:
@@ -34,6 +42,7 @@ for line in linkfile:
     for s in tup:
         if not s in node_urls:
             print "Warning: not in nodes.txt: "+s
+    link_species[(stuff[0], stuff[1], num)] = ' '.join(stuff[3:]).strip()
 
 linkfile.close()
 
@@ -53,7 +62,7 @@ def thumbnailify(name, url):
         pass
 
     debug = True
-    WIDTH, HEIGHT = 100, 100
+    WIDTH, HEIGHT = 50, 50
 
     if debug:
         print url
@@ -61,10 +70,10 @@ def thumbnailify(name, url):
     img = Image.open(infile)
     im = img.copy()
     if im.size[0] >= im.size[1]:
-        im = im.resize((HEIGHT * im.size[0]/im.size[1], HEIGHT))
+        im = im.resize((HEIGHT * im.size[0]/im.size[1], HEIGHT), Image.ANTIALIAS)
         im = im.crop(((im.size[0]-WIDTH)/2, 0, (im.size[0]+WIDTH)/2, HEIGHT))
     else:
-        im = im.resize((WIDTH, WIDTH*im.size[1]/im.size[0]))
+        im = im.resize((WIDTH, WIDTH*im.size[1]/im.size[0]), Image.ANTIALIAS)
         im = im.crop((0, (im.size[1]-HEIGHT)/2, WIDTH, (im.size[1]+HEIGHT)/2))
     if im.mode != "RGB":
         im = im.convert("RGB")
@@ -89,7 +98,7 @@ def toenailify(name, url):
     infile = cStringIO.StringIO(urllib.urlopen(url).read())
     img = Image.open(infile)
     im = img.copy()
-    im = im.resize((WIDTH, WIDTH*im.size[1]/im.size[0]))
+    im = im.resize((WIDTH, WIDTH*im.size[1]/im.size[0]), Image.ANTIALIAS)
     if im.mode != "RGB":
         im = im.convert("RGB")
     im.save(outfilename)
@@ -109,15 +118,22 @@ def token(link):
 def text(link):
     return link[1]+"-mimicking "+link[0]
 
+def descriptorify(name, species):
+    species = species.strip()
+    if len(species) == 0:
+        return name
+    else:
+        return "<i>%s</i>, %s" % (species, name)
+
 ##Output
 for line in filein:
     if line.strip() == "//ADDNODES":
         #graph.addNode('anvaka', {url:'https://secure.gravatar.com/avatar/91bad8ceeec43ae303790f8fe238164b', name:'whatever'});
         for node in node_urls:
-            writeln("graph.addNode('"+node+"', {url:'"+thumbnailify(node, node_urls[node])+"', name: '"+node+"'});")
+            writeln("graph.addNode('"+node+"', {url:'"+thumbnailify(node, node_urls[node])+"', name: '"+node+"', source: '"+node_urls[node]+"', descriptor: '"+descriptorify(node, node_species[node])+"'});")
             #writeln("graph.addNode('"+node+"', {url:'"+thumbnailify(node, node_urls[node])+"'});")
         for link in link_urls:
-            writeln("graph.addNode('"+token(link)+"', {url:'"+thumbnailify(token(link), link_urls[link])+"', name: '"+text(link)+"'});")
+            writeln("graph.addNode('"+token(link)+"', {url:'"+thumbnailify(token(link), link_urls[link])+"', name: '"+text(link)+"', source: '"+link_urls[link]+"', descriptor: '"+descriptorify(text(link), link_species[link])+"'});")
             #writeln("graph.addNode('"+token(link)+"', {url:'"+thumbnailify(token(link), link_urls[link])+"'});")
 
     elif line.strip() == "//ADDLINKS":
@@ -150,12 +166,16 @@ for line in filein:
         writeln("pictionary = " + json.dumps(jsonout) + ";")
 
     elif line.strip() == "//PRELOADER":
-        for node in node_urls:
-            url = node_urls[node]
+        for node, url in node_urls:
             writeln("<img src=\""+toenailify(node, url)+"\" width=\"1\" height=\"1\" />")
-        for link in link_urls:
-            url = link_urls[link]
+        for link, url in link_urls:
             writeln("<img src=\""+toenailify(token(link), url)+"\" width=\"1\" height=\"1\" />")
+
+    elif line.strip() == "//PRELOADER_JS":
+        for node, url in node_urls.items():
+            writeln("document.getElementById(\"preloader\").style.background = \"url("+toenailify(node, url)+") no-repeat -9999px -9999px\";")
+        for link, url in link_urls.items():
+            writeln("document.getElementById(\"preloader\").style.background = \"url("+toenailify(token(link), url)+") no-repeat -9999px -9999px\";")
 
     else:
         write(line)
